23029c2b4bed2560c66e6471c40ca649b1c519c8
rdmcoder123 <dhatshinaklu@gmail.com>
2024-12-13 17:52:32 +0530

    Add new skin color styles and update EmailJS dependency

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "Hi, I am an ECE student with good knowledge in IoT, machine learning, DSPs, and communications. I specialize in integrating hardware and software to create innovative real-time applications. My ability to bridge these domains allows me to design intelligent, efficient, and impactful solutions. As a fast learner and technology enthusiast, I am always eager to explore new fields, enhance my skill set, and contribute to solving real-world challenges with creativity and technical expertise"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
